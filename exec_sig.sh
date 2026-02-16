9ae54798ebdd6faaf5796bd1fa9afb59123d6fe1
Ioannis Xiotidis <ixiotidi@atlas-nextgen-wp21.dyndns.cern.ch>
2026-02-16 19:16:36 +0100

    Fixing generation to 14TeV

--- exec_sig.sh
+++ exec_sig.sh
@@ -7,5 +7,5 @@ for ((i=start; i<=end; i++));
 do
     offset=$(( i*10 ))
     rng_seed=$(( i+1 ))
-    python run.py -n 10 -O /vols/drive2/ixiotidi/ttbar/test_gen/job_$i -p_gen "$offset" -p 1,-1 -pb 1 -sp 0 -rng "$rng_seed" -index_offset 0 -config jetnet_config/ttbar_config.py --steps "generation,pileup,simulation"
+    python run.py -n 10 -O /vols/drive2/ixiotidi/jz/job_$i -p_gen "$offset" -p 1,-1 -pb 1 -sp 0 -rng "$rng_seed" -index_offset 0 -config jetnet_config/jz_config.py --steps "generation,pileup,simulation"
 done
